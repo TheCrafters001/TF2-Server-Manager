a6958d097bb2e1046ab6ebedea71e9d631f8d51e
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-06-24 18:08:07 -0400

    Fix A Bunch Of Crap
    So, it seems like I screwed up something. Well, here is my fix.

--- src/TF2 Server Manager/Server_Manager_Main.Designer.vb
+++ src/TF2 Server Manager/Server_Manager_Main.Designer.vb
@@ -22,56 +22,10 @@ Partial Class Server_Manager_Main
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Server_Manager_Pages = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.Server_Manager_Pages.SuspendLayout()
-        Me.SuspendLayout()
-        '
-        'Server_Manager_Pages
-        '
-        Me.Server_Manager_Pages.Controls.Add(Me.TabPage1)
-        Me.Server_Manager_Pages.Controls.Add(Me.TabPage2)
-        Me.Server_Manager_Pages.Location = New System.Drawing.Point(12, 12)
-        Me.Server_Manager_Pages.Name = "Server_Manager_Pages"
-        Me.Server_Manager_Pages.SelectedIndex = 0
-        Me.Server_Manager_Pages.Size = New System.Drawing.Size(708, 609)
-        Me.Server_Manager_Pages.TabIndex = 0
-        '
-        'TabPage1
-        '
-        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(700, 583)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "TabPage1"
-        Me.TabPage1.UseVisualStyleBackColor = True
-        '
-        'TabPage2
-        '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(192, 74)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
-        Me.TabPage2.UseVisualStyleBackColor = True
-        '
-        'Server_Manager_Main
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        components = New System.ComponentModel.Container()
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(732, 633)
-        Me.Controls.Add(Me.Server_Manager_Pages)
-        Me.Name = "Server_Manager_Main"
-        Me.Text = "Server_Manager_Main"
-        Me.Server_Manager_Pages.ResumeLayout(False)
-        Me.ResumeLayout(False)
-
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Text = "Form1"
     End Sub
 
-    Friend WithEvents Server_Manager_Pages As Windows.Forms.TabControl
-    Friend WithEvents TabPage1 As Windows.Forms.TabPage
-    Friend WithEvents TabPage2 As Windows.Forms.TabPage
 End Class
