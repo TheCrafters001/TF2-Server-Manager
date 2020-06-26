17750c970b619445f2c668cb9530fffa0dcf831f
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-06-25 21:11:27 -0400

    Update Stuff

--- src/TF2 Server Manager Console/Module1.vb
+++ src/TF2 Server Manager Console/Module1.vb
@@ -12,19 +12,20 @@
     Dim folder As String
 
     Sub Main()
-        Menu()
-    End Sub
-
-    Sub Menu()
         Console.WriteLine("==============================")
         Console.WriteLine("= TF2 Server Manager Console =")
         Console.WriteLine("= By TheCrafters001          =")
         Console.WriteLine("==============================")
+        Menu()
+    End Sub
+
+    Sub Menu()
         Console.WriteLine("")
         Console.WriteLine("Select an option")
         Console.WriteLine("1. Start Server With Saved Settings")
         Console.WriteLine("2. Start Server With New Settings  ")
         Console.WriteLine("3. Select Settings to Save         ")
+        Console.WriteLine("4. About                           ")
         Console.WriteLine("")
         Console.WriteLine("")
         Console.Write("Select a Number: ")
@@ -43,9 +44,13 @@
                 sv_alltalk = My.Settings.sv_alltalk
                 Server_Manager.StartServerWithSettings(Server_Name, Startup_Map, folder, Max_Players, Server_Port, SV_PURE, SV_LAN, sv_region, mp_disable_respawn_times, sv_alltalk)
             ElseIf MenuSelect = 2 Then
-
+                Server_Manager.StartServerWithNewSettings()
             ElseIf MenuSelect = 3 Then
 
+            ElseIf MenuSelect = 4 Then
+                About()
+                Console.WriteLine("")
+                Menu()
             End If
         Catch ex As Exception
             Console.Clear()
@@ -53,8 +58,30 @@
             Console.WriteLine("")
             Console.WriteLine(ex)
             Console.WriteLine("")
-            Main()
+            Menu()
         End Try
+    End Sub
+
+    Private Sub About()
+        Console.WriteLine("A Simple TF2 Server Manager.")
+        Console.WriteLine("Copyright (C) 2020 TheCrafters001")
+        Console.WriteLine("")
+        Console.WriteLine("This program is free software: you can redistribute it and/or modify")
+        Console.WriteLine("it under the terms of the GNU General Public License as published by")
+        Console.WriteLine("the Free Software Foundation, either version 3 of the License, or")
+        Console.WriteLine("(at your option) any later version.")
+        Console.WriteLine("")
+        Console.WriteLine("This program is distributed in the hope that it will be useful,")
+        Console.WriteLine("but WITHOUT ANY WARRANTY; without even the implied warranty of")
+        Console.WriteLine("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the")
+        Console.WriteLine("GNU General Public License for more details.")
+        Console.WriteLine("")
+        Console.WriteLine("You should have received a copy of the GNU General Public License")
+        Console.WriteLine("along with this program.  If not, see <https://www.gnu.org/licenses/>.")
+        Console.WriteLine("")
+        '
+        ' Display full license. DO NOT REMOVE
+        '
     End Sub
 
 End Module
--- src/TF2 Server Manager Console/Server_Manager.vb
+++ src/TF2 Server Manager Console/Server_Manager.vb
@@ -17,7 +17,57 @@
         Console.WriteLine("sv_alltalk              : " & sv_alltalk.ToString)
         Console.WriteLine("Folder Location         : " & Folder)
         Console.WriteLine("")
-        Module1.Main()
+        Module1.Menu()
+    End Sub
+
+    Public Shared Sub StartServerWithNewSettings()
+        Dim Server_Name As String
+        Dim Startup_Map As String
+        Dim Max_Players As Integer
+        Dim Server_Port As Integer
+        Dim SV_PURE As Integer
+        Dim SV_LAN As Integer
+        Dim sv_region As Integer
+        Dim mp_disable_respawn_times As Integer
+        Dim sv_alltalk As Integer
+        Dim folder As String
+        Console.WriteLine("========================================")
+        Console.WriteLine("= Start Server With New Settings       =")
+        Console.WriteLine("========================================")
+        Console.WriteLine("Starting with the following Settings.")
+        Console.WriteLine("")
+        Console.Write("Server Name             : ")
+        Server_Name = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("Startup Map             : ")
+        Startup_Map = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("Max Players             : ")
+        Max_Players = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("Server Port             : ")
+        Server_Port = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("sv_pure                 : ")
+        SV_PURE = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("sv_lan                  : ")
+        SV_LAN = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("sv_region               : ")
+        sv_region = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("mp_disable_respawn_times: ")
+        mp_disable_respawn_times = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("sv_alltalk              : ")
+        sv_alltalk = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("Folder Location         : ")
+        folder = Console.ReadLine()
+        Console.WriteLine("")
+        Console.WriteLine("Failed to save settings: Saving Coming Soon")
+        Module1.Menu()
     End Sub
 
 End Class
