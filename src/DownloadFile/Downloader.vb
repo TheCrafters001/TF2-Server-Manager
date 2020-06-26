84454a6f88d4165ec6cd978a3f6be45db66db77c
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-06-25 21:55:27 -0400

    DownloadFile Component.

---
+++ src/DownloadFile/Downloader.vb
@@ -0,0 +1,33 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.HttpStyleUriParser
+Imports System.Net
+Imports System.Net.Http
+
+Public Class Downloader
+    'This will NOT download anything harmful to your computer.
+    'All Files Downloaded will come from VALVe. Meaning there
+    'is nothing to worry about!
+
+    'Please do NOT alter these files, as they are required, and
+    'I don't want to be blamed for any wrong doing. Any pull request (PRs)
+    'Will be ignored and closed.
+    '
+    '- TheCrafters001
+    Public Shared Sub SteamCMD_DL(ByVal fileURL As String, ByVal fileOutput As String)
+        If (Not System.IO.Directory.Exists("C:\Server Manager\SteamCMD\")) Then
+            System.IO.Directory.CreateDirectory("C:\Server Manager\SteamCMD\")
+        End If
+        Using client = New WebClient()
+            client.DownloadFile(New Uri(fileURL), "C:\Server Manager\SteamCMD\" & fileOutput)
+        End Using
+    End Sub
+
+    Public Shared Sub Unzip(ByVal zipName As String)
+        Dim startPath As String = "C:\Server Manager\SteamCMD\"
+        Dim zipPath As String = "C:\Server Manager\SteamCMD\" & zipName & ".zip"
+        Dim extractPath As String = "C:\Server Manager\SteamCMD\"
+        ZipFile.ExtractToDirectory(zipPath, extractPath)
+    End Sub
+
+End Class
---
+++ src/DownloadFile/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DownloadFile")>
+<Assembly: AssemblyDescription("Download Files from Trusted Sources.")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DownloadFile")>
+<Assembly: AssemblyCopyright("Copyright © TheCrafters001 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1105ab31-4be8-41a3-b154-2993a0ee86ab")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
