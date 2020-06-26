8c55cef074229128cabfbb3da4c8b6da6f768f7f
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-06-25 22:34:37 -0400

    Update Server_Manager.vb

--- src/TF2 Server Manager Console/Server_Manager.vb
+++ src/TF2 Server Manager Console/Server_Manager.vb
@@ -21,7 +21,14 @@ Public Class Server_Manager
         Console.WriteLine("")
         Console.WriteLine("")
         If My.Computer.FileSystem.FileExists("C:\Server Manager\SteamCMD\SteamCMD.exe") Then
-            ' Do Nothing For Now
+            Try
+                Server_Runner.Run(Server_Name, startup_map, Folder, Max_Players, Server_Port, SV_PURE, SV_LAN, sv_region, mp_disable_respawn_times, sv_alltalk)
+            Catch ex As Exception
+                Console.WriteLine("Failed.")
+                Console.WriteLine("")
+                Console.WriteLine(ex)
+                Module1.Menu()
+            End Try
         Else
             Console.WriteLine("SteamCMD was not found on your system. Would you like to download it? (Internet required)")
             Console.WriteLine("")
@@ -118,10 +125,118 @@ Public Class Server_Manager
         Console.Write("Folder Location         : ")
         folder = Console.ReadLine()
         Console.WriteLine("")
-        Console.WriteLine("Failed to save settings: Saving Coming Soon")
+        Console.WriteLine("Saving Settings...")
+        My.Settings.Save()
+        Console.WriteLine("Settings Saved")
+        If My.Computer.FileSystem.FileExists("C:\Server Manager\SteamCMD\SteamCMD.exe") Then
+            Try
+                Server_Runner.Run(Server_Name, Startup_Map, folder, Max_Players, Server_Port, SV_PURE, SV_LAN, sv_region, mp_disable_respawn_times, sv_alltalk)
+            Catch ex As Exception
+                Console.WriteLine("Failed.")
+                Console.WriteLine("")
+                Console.WriteLine(ex)
+                Module1.Menu()
+            End Try
+        Else
+            Console.WriteLine("SteamCMD was not found on your system. Would you like to download it? (Internet required)")
+            Console.WriteLine("")
+            Console.Write("Enter a selection [Y/n] = ")
+            Dim SteamCMDDownloader As String = Console.ReadLine()
+            SteamCMDDownloader = SteamCMDDownloader.ToLower
+            If SteamCMDDownloader = "y" Then
+                Console.WriteLine("Okay. Downloading SteamCMD...")
+                Try
+                    SteamCMD_DL("https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip", "steamcmd.zip")
+                Catch ex As Exception
+                    Console.WriteLine("Failed.")
+                    Console.WriteLine("")
+                    Console.WriteLine(ex)
+                    Module1.Menu()
+                End Try
+                Console.WriteLine("Downloaded. Unzipping SteamCMD...")
+                Try
+                    Unzip("steamcmd")
+                Catch ex As Exception
+                    Console.WriteLine("Failed.")
+                    Console.WriteLine("")
+                    Console.WriteLine(ex)
+                    Module1.Menu()
+                End Try
+                Console.WriteLine("SteamCMD installed! Erasing zip file...")
+                Try
+                    My.Computer.FileSystem.DeleteFile("C:\Server Manager\SteamCMD\steamcmd.zip")
+                Catch ex As Exception
+                    Console.WriteLine("Failed.")
+                    Console.WriteLine("")
+                    Console.WriteLine(ex)
+                    Module1.Menu()
+                End Try
+                Console.WriteLine("Done.")
+                Try
+                    Server_Runner.Run(Server_Name, Startup_Map, folder, Max_Players, Server_Port, SV_PURE, SV_LAN, sv_region, mp_disable_respawn_times, sv_alltalk)
+                Catch ex As Exception
+                    Console.WriteLine("Failed.")
+                    Console.WriteLine("")
+                    Console.WriteLine(ex)
+                    Module1.Menu()
+                End Try
+            ElseIf SteamCMDDownloader = "n" Then
+                Console.WriteLine("Okay, Not downloading SteamCMD. Operation will now cancel.")
+            End If
+        End If
         Module1.Menu()
     End Sub
 
-
+    Public Shared Sub SettingsNoStart()
+        Dim Server_Name As String
+        Dim Startup_Map As String
+        Dim Max_Players As Integer
+        Dim Server_Port As Integer
+        Dim SV_PURE As Integer
+        Dim SV_LAN As Integer
+        Dim sv_region As Integer
+        Dim mp_disable_respawn_times As Integer
+        Dim sv_alltalk As Integer
+        Dim folder As String
+        Console.WriteLine("========================================")
+        Console.WriteLine("= Start Server With New Settings       =")
+        Console.WriteLine("========================================")
+        Console.WriteLine("Starting with the following Settings.")
+        Console.WriteLine("")
+        Console.Write("Server Name             : ")
+        Server_Name = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("Startup Map             : ")
+        Startup_Map = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("Max Players             : ")
+        Max_Players = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("Server Port             : ")
+        Server_Port = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("sv_pure                 : ")
+        SV_PURE = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("sv_lan                  : ")
+        SV_LAN = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("sv_region               : ")
+        sv_region = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("mp_disable_respawn_times: ")
+        mp_disable_respawn_times = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("sv_alltalk              : ")
+        sv_alltalk = Console.ReadLine()
+        Console.WriteLine("")
+        Console.Write("Folder Location         : ")
+        folder = Console.ReadLine()
+        Console.WriteLine("")
+        Console.WriteLine("Saving Settings...")
+        My.Settings.Save()
+        Console.WriteLine("Settings Saved")
+        Module1.Menu()
+    End Sub
 
 End Class
